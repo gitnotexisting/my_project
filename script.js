16d9fc162c566e9aaac67e255af6eed7cacce6bc
gitnotexisting <tenevenb@gmail.com>
2024-07-02 00:09:10 +0300

    add DOTS

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let img_array = ['img/arch1.png', 'img/arch2.png', 'img/arch3.png'];
-let img_id =  document.getElementById("img-id");
+const img_id =  document.getElementById("img-id");
 let x = 0;
 img_id.src = img_array[x];
 
-let forward = document.getElementById("forward");
-let backward = document.getElementById("backward");
+const forward = document.getElementById("forward");
+const backward = document.getElementById("backward");
 
 forward.addEventListener('click', () => {    
     if (x < img_array.length-1) {
@@ -23,4 +23,23 @@ backward.addEventListener('click', () => {
 
 
 // Generate "dots" to show current picture position in Carousel
+
+const dots_container = document.getElementById("dots-con");
+
+function createDotElement(number) {
+    for (let index = 0; index < number; index++) {
+        let tag = document.createElement("div")
+        tag.style.cssText = `
+        width: 20px;
+        height: 20px;
+        margin: 10px;
+        border: 1px solid black;
+        border-radius: 50%
+        `;
+        dots_container.appendChild(tag);
+    }    
+}
+
+createDotElement(img_array.length);
+
 
