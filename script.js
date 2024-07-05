fac072b74d30f6951ec28d7ae77f733606534b2b
gitnotexisting <tenevenb@gmail.com>
2024-07-05 21:52:59 +0300

    Starting SNOW

--- script.js
+++ script.js
@@ -8,38 +8,67 @@ const backward = document.getElementById("backward");
 
 forward.addEventListener('click', () => {    
     if (x < img_array.length-1) {
+        disableActiveDot();
         x += 1;
-        img_id.src = img_array[x];        
+        img_id.src = img_array[x];
+        enableActiveDot();     
     }
 });
 
 backward.addEventListener('click', () => {    
     if (x > 0) {
+        disableActiveDot();
         x -= 1;
-        img_id.src = img_array[x];    
+        img_id.src = img_array[x];
+        enableActiveDot();
     }
     
 });
 
 
-// Generate "dots" to show current picture position in Carousel
+//--------- Generate "dots" to show current picture position in Carousel
 
-const dots_container = document.getElementById("dots-con");
+// const dots_container = document.getElementById("dots-con");
 
-function createDotElement(number) {
-    for (let index = 0; index < number; index++) {
-        let tag = document.createElement("div")
-        tag.style.cssText = `
-        width: 20px;
-        height: 20px;
-        margin: 10px;
-        border: 1px solid black;
-        border-radius: 50%
-        `;
-        dots_container.appendChild(tag);
-    }    
+// function createDotElement(number) {
+//     for (let index = 0; index < number; index++) {
+//         let tag = document.createElement("div")
+//         tag.style.cssText = `
+//         width: 20px;
+//         height: 20px;
+//         margin:  10px;
+//         border: 1px solid black;
+//         border-radius: 50%
+//         `;
+//         dots_container.appendChild(tag);
+//     }    
+// }
+
+// createDotElement(img_array.length);
+
+//----------- Active dot tracker
+
+let dots = document.getElementsByClassName("dot");
+function enableActiveDot() {
+    dots[x].className += " dot-active";   
+}
+enableActiveDot();
+function disableActiveDot() {
+    dots[x].className = " dot";
 }
 
-createDotElement(img_array.length);
+//----------- Header snow
+let showfly_color = ['blue', 'red', 'green', 'yellow', 'black'];
+let header = document.getElementsByClassName("header");
+let snowfly = document.createElement("p");
+let num = Math.floor(Math.random()*4);
+snowfly.textContent = "*";
+snowfly.style.cssText =  `
+    font-size: 18px;
+    color: ${showfly_color[num]};
+`;
 
-
+function createSnowfly() {
+    header[header.length-1].appendChild(snowfly);
+}
+createSnowfly();
